45b9c3d994b96f6131d125d578e731918f4cdc18
SagawaYasumasa <feelingyas@gmail.com>
2023-01-29 11:08:50 +0900

    add GPS unit

--- ArduinoHello.ino
+++ ArduinoHello.ino
@@ -3,56 +3,63 @@
 
 #include "private.h"
 #define DATA_FILE_NAME  "/json.txt"
-/*
-*******************************************************************************
-* Copyright (c) 2021 by M5Stack
-*                  Equipped with M5Core2 sample source code
-*                          配套  M5Core2 示例源代码
-* Visit for more information: https://docs.m5stack.com/en/core/core2
-* 获取更多资料请访问: https://docs.m5stack.com/zh_CN/core/core2
-*
-* Describe: Wifi scan.  wifi扫描
-* Date: 2021/7/28
-*******************************************************************************
-*/
+
 #include <M5Core2.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <TinyGPSPlus.h>
+
 #include "ssiddata.h"
 #include "debugTool.h"
 
+static const uint32_t GPSBaud = 9600;
+static const int  Font1Height = 8;      // Font=1, Adafruit 8 pixels ascii font
 hw_timer_t *timer = NULL;
 volatile unsigned long gTimerCounter = 0;
 
+//Creat The TinyGPS++ object.
+TinyGPSPlus gps;
+// The serial connection to the GPS device.
+HardwareSerial ss(2);
+
+SsidData ssidData;
+
+
 void setup() {
   M5.begin();  //Init M5Stack.
+  ss.begin(GPSBaud, SERIAL_8N1, 33,32);  //It requires the use of SoftwareSerial, and assumes that you have a 4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).  
+
   // Init Timer
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer,&onTimer,true);
   timerAlarmWrite(timer,1000*1000,true);      // 1000msec interval
   timerAlarmEnable(timer);
   
-  WiFi.mode(
-      WIFI_STA);  // Set WiFi to station mode and disconnect from an AP if it was previously connected.  将WiFi设置为站模式，如果之前连接过AP，则断开连接
-  WiFi.disconnect();  //Turn off all wifi connections.  关闭所有wifi连接
-  delay(100);         //100 ms delay.  延迟100ms
-  M5.Lcd.print("WIFI SCAN\n");  //Screen print string.  屏幕打印字符串
-  M5.Lcd.printf("version %d.%02d\n",MAJOR_VERSION,MINOR_VERSION);
-  Serial.printf("Hello, %s.\n",MYNAME);
+  WiFi.mode(WIFI_STA);  // Set WiFi to station mode and disconnect from an AP if it was previously connected. 
+  WiFi.disconnect();  //Turn off all wifi connections.
+  delay(100);         //100 ms delay.
+  M5.Lcd.setCursor(0,0 * Font1Height,1);
+  M5.Lcd.printf("Wi-Fi Clawer for M5Stack. version %d.%02d \n",MAJOR_VERSION,MINOR_VERSION);
+  M5.Lcd.printf("User name:%s\n",MYNAME);
+//  ssidData.SsidData();
   delay(1000);
 }
 
 void onTimer(){
   gTimerCounter++;
-//  Serial.printf("onTimer %d\n",gTimerCounter);
 }
 
 void loop() {
-  M5.Lcd.setCursor(0, 0);  //Set the cursor at (0,0).  将光标设置在(0,0)处
-  M5.Lcd.println("Please press Btn.A to (re)scan");
-  M5.update();  //Check the status of the key.  检测按键的状态
+  M5.Lcd.setCursor(0, 2 * Font1Height, 1);
+  printGpsSatellites(gps.satellites.value(),gps.satellites.isValid());
+  printGpsHdop(gps.hdop.value(),gps.hdop.isValid());  
+  printGpsDateTime(gps.date,gps.time);  
+  printGpsLocation(gps.location.lat(),gps.location.lng(),gps.location.isValid());
 
-  SsidData ssidData = SsidData();
+  M5.Lcd.println("Please press Btn.A to (re)scan");
+  M5.update();  //Check the status of the key.
+
+//  SsidData ssidData = SsidData();
 
   // Buton A /////////////////////////////////////////////////////////////
   if (M5.BtnA.isPressed()) {  //If button A is pressed.
@@ -86,7 +93,7 @@ void loop() {
     }
     Serial.printf("\nScan result numberOfWifi=%d,gpsStatus=%d\n",numberOfWifi,gpsStatus);
 
-    if ((numberOfWifi == WIFI_SCAN_RUNNING) ||(numberOfWifi == WIFI_SCAN_FAILED)) {  //If no network is found.
+    if ((numberOfWifi == WIFI_SCAN_RUNNING) ||(numberOfWifi == WIFI_SCAN_FAILED) ||(numberOfWifi == 0)) {  //If no network is found.
       M5.Lcd.println("\nno networks found");
     } else {  //If have network is found.
       String json="";
@@ -109,7 +116,7 @@ void loop() {
       }
       writeRecord((char *)json.c_str());
     }
-    delay(1000);
+    delay(500);
   }
   // Buton B /////////////////////////////////////////////////////////////
   if (M5.BtnB.isPressed()) {  //If button B is pressed.
@@ -135,6 +142,72 @@ void loop() {
     deleteFile();
     Serial.printf("function:loop, ButtunC Pressed-exit\n");
   }
+
+  smartDelay(200);
+  if (millis() > 5000 && gps.charsProcessed() < 10){
+    M5.Lcd.println(F("No GPS data received: check wiring"));  
+  }
+}
+// This custom version of delay() ensures that GPS objects work properly.
+static void smartDelay(unsigned long ms) {
+  unsigned long start = millis();
+  do {
+    while (ss.available()){
+      gps.encode(ss.read());
+    }
+  } while (millis() - start < ms);
+}
+// Print number of acquired satellites 
+static void printGpsSatellites(int numberOfSatellites, bool valid){
+  if(valid){
+    M5.Lcd.printf("Number of acquired satellite(s) : %d     \n",numberOfSatellites);
+  } else {
+    M5.Lcd.printf("Invalid number of acquired satellite      \n");
+  }
+}
+// Print horizontal dilution of Precision(HDOP)
+static void printGpsHdop(int hdop, bool valid){
+  if(valid){
+    M5.Lcd.printf("Horizontal dilution of precision(HDOP) : %d     \n",hdop);
+  } else {
+    M5.Lcd.printf("Invalid HDOP                                     \n");
+  }
+}
+// Print Date and Time(UTC+0000)
+static void printGpsDateTime(TinyGPSDate &d, TinyGPSTime &t) {
+  char tmpDate[10+1]; // "YYYY/MM/DD"
+  char tmpTime[8+1];  //  "hh:mm:ss"
+
+  memset(tmpDate,0x00,sizeof(tmpDate));
+  memset(tmpTime,0x00,sizeof(tmpTime));
+  if(d.isValid()){
+    sprintf(tmpDate,"%04d/%02d/%02d",d.year(),d.month(),d.day());    
+  } else {
+    sprintf(tmpDate,"****/**/**");    
+  }
+  if(t.isValid()){
+    sprintf(tmpTime,"%02d/%02d/%02d",t.hour(),t.minute(),t.second());    
+  } else {
+    sprintf(tmpTime,"**:**:**");    
+  }
+  M5.Lcd.printf("%s %s (UTC+0000)\n",tmpDate,tmpTime);
+  smartDelay(0);
+}
+// Print Latitude and Longitude
+static void printGpsLocation(double latitude, double longitude, bool valid){
+  char  tmpLat[11+1];
+  char  tmpLng[11+1];
+  memset(tmpLat,0x00,sizeof(tmpLat));
+  memset(tmpLng,0x00,sizeof(tmpLng));
+  if(valid){
+    dtostrf(latitude,11,6,tmpLat);
+    dtostrf(longitude,11,6,tmpLng);
+  } else {
+    sprintf(tmpLat," ***.******");
+    sprintf(tmpLng," ***.******");
+  }
+  M5.Lcd.printf("Latitude :%s\n",tmpLat);
+  M5.Lcd.printf("Longitude:%s\n",tmpLng); 
 }
 bool connectWiFi(){
   bool ret = false;
